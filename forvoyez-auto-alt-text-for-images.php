6f296efb8d29e2965b3a99a187145f25e980ee54
Cinquin Andy <andy.cinquin@infotel.com>
2024-07-03 17:26:45 +0200

    ✨ Add table to display image details on settings page
    Added a table to the settings page displaying images, titles, alt text, and captions fetched from attachments using WP_Query.

--- forvoyez-auto-alt-text-for-images.php
+++ forvoyez-auto-alt-text-for-images.php
@@ -31,7 +31,37 @@ function forvoyez_settings_page() {
     <div class="wrap">
         <h1>Welcome to Auto Alt Text for Images</h1>
         <p>This plugin will help you automatically generate alt text for your images using the ForVoyez API.</p>
-        <p>More settings and options will be available in future updates.</p>
+        <table class="wp-list-table widefat fixed striped">
+            <thead>
+            <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Alt Text</th>
+                <th>Caption</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $args = array(
+                'post_type' => 'attachment',
+                'post_mime_type' => 'image',
+                'post_status' => 'inherit',
+                'posts_per_page' => -1,
+            );
+            $query_images = new WP_Query($args);
+            foreach ($query_images->posts as $image) :
+                $image_url = wp_get_attachment_url($image->ID);
+                $image_alt = get_post_meta($image->ID, '_wp_attachment_image_alt', true);
+                ?>
+                <tr>
+                    <td><img src="<?php echo esc_url($image_url); ?>" style="max-width: 100px; height: auto;"></td>
+                    <td><?php echo esc_html($image->post_title); ?></td>
+                    <td><?php echo esc_html($image_alt); ?></td>
+                    <td><?php echo esc_html($image->post_excerpt); ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
     <?php
 }
