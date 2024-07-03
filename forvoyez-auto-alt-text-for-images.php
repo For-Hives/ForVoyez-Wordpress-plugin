c23c4c5e5496a8022a6d6db1c377cf5905073528
Cinquin Andy <andy.cinquin@infotel.com>
2024-07-03 17:01:36 +0200

    base

---
+++ forvoyez-auto-alt-text-for-images.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Auto Alt Text for Images
+ * Description: Automatically generate alt text and SEO metadata for images using ForVoyez API.
+ * Version: 1.0.0
+ * Author: ForVoyez
+ * Author URI: https://forvoyez.com
+ * Text Domain: forvoyez-auto-alt-text-for-images
+ */
+
