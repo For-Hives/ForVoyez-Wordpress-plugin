32fa53d2ba7c597e7a5e33952ad65fe7b4488fbc
Cinquin Andy <andy.cinquin@infotel.com>
2024-07-03 17:14:56 +0200

    ✨ Add menu item to WordPress admin
    Prevent direct file access and add menu item with settings page for Auto Alt Text plugin.

--- forvoyez-auto-alt-text-for-images.php
+++ forvoyez-auto-alt-text-for-images.php
@@ -8,3 +8,30 @@
  * Text Domain: forvoyez-auto-alt-text-for-images
  */
 
+// Prevent direct access to the file
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Add menu item to the WordPress admin
+function forvoyez_add_menu_item() {
+    add_options_page(
+        'Auto Alt Text Settings',  // Page title
+        'Auto Alt Text',           // Menu title
+        'manage_options',          // Capability required to see this option
+        'forvoyez-auto-alt-text',  // Menu slug
+        'forvoyez_settings_page'   // Function to output the content for this page
+    );
+}
+add_action('admin_menu', 'forvoyez_add_menu_item');
+
+// Create the settings page
+function forvoyez_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>Welcome to Auto Alt Text for Images</h1>
+        <p>This plugin will help you automatically generate alt text for your images using the ForVoyez API.</p>
+        <p>More settings and options will be available in future updates.</p>
+    </div>
+    <?php
+}
