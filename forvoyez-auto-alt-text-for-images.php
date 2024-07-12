615c906d719631aaf5d4eecca2671c0b102a9227
Cinquin Andy <cinquin.andy@gmail.com>
2024-07-12 12:02:41 +0200

    Update: Refactor image rendering and filter display - Moved `display_filters` and `render_image_item` methods to a new class `Forvoyez_Image_Renderer` - Improved code organization by separating concerns for better readability and maintainability

--- forvoyez-auto-alt-text-for-images.php
+++ forvoyez-auto-alt-text-for-images.php
@@ -8,20 +8,16 @@
  * Text Domain: forvoyez-auto-alt-text-for-images
  */
 
-if (!defined('ABSPATH')) {
-    exit;
-}
+defined('ABSPATH') || exit;
 
-define('FORVOYEZ_PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('FORVOYEZ_PLUGIN_URL', plugin_dir_url(__FILE__));
-
+require_once plugin_dir_path(__FILE__) . 'includes/forvoyez-constants.php';
+require_once FORVOYEZ_PLUGIN_DIR . 'includes/forvoyez-helpers.php';
 require_once FORVOYEZ_PLUGIN_DIR . 'includes/class-forvoyez-admin.php';
 require_once FORVOYEZ_PLUGIN_DIR . 'includes/class-forvoyez-api.php';
 require_once FORVOYEZ_PLUGIN_DIR . 'includes/class-forvoyez-image-processor.php';
 require_once FORVOYEZ_PLUGIN_DIR . 'includes/class-forvoyez-settings.php';
 
-function forvoyez_init()
-{
+function forvoyez_init() {
     $admin = new Forvoyez_Admin();
     $admin->init();
 
@@ -35,9 +31,47 @@ function forvoyez_init()
     $settings->init();
 }
 
-function forvoyez_get_api_key(){
-    $settings = new Forvoyez_Settings();
-    return $settings->get_api_key();
-}
-
 add_action('plugins_loaded', 'forvoyez_init');
+
+///**
+// * Plugin Name: Auto Alt Text for Images
+// * Description: Automatically generate alt text and SEO metadata for images using ForVoyez API.
+// * Version: 1.0.0
+// * Author: ForVoyez
+// * Author URI: https://forvoyez.com
+// * Text Domain: forvoyez-auto-alt-text-for-images
+// */
+//
+//if (!defined('ABSPATH')) {
+//    exit;
+//}
+//
+//define('FORVOYEZ_PLUGIN_DIR', plugin_dir_path(__FILE__));
+//define('FORVOYEZ_PLUGIN_URL', plugin_dir_url(__FILE__));
+//
+//require_once FORVOYEZ_PLUGIN_DIR . 'includes/class-forvoyez-admin.php';
+//require_once FORVOYEZ_PLUGIN_DIR . 'includes/class-forvoyez-api.php';
+//require_once FORVOYEZ_PLUGIN_DIR . 'includes/class-forvoyez-image-processor.php';
+//require_once FORVOYEZ_PLUGIN_DIR . 'includes/class-forvoyez-settings.php';
+//
+//function forvoyez_init()
+//{
+//    $admin = new Forvoyez_Admin();
+//    $admin->init();
+//
+//    $api = new Forvoyez_API();
+//    $api->init();
+//
+//    $image_processor = new Forvoyez_Image_Processor();
+//    $image_processor->init();
+//
+//    $settings = new Forvoyez_Settings();
+//    $settings->init();
+//}
+//
+//function forvoyez_get_api_key(){
+//    $settings = new Forvoyez_Settings();
+//    return $settings->get_api_key();
+//}
+//
+//add_action('plugins_loaded', 'forvoyez_init');
