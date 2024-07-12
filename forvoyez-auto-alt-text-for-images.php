b1ac22e9f699e2505654027ebe1d2940c64668eb
Cinquin Andy <cinquin.andy@gmail.com>
2024-07-12 11:58:37 +0200

    Update: Refactor image alt text functions - Moved 'forvoyez_count_incomplete_images' function to a separate helpers file for better organization and maintainability - Defined constants for plugin directory path and URL in a new constants file
    These changes were made to improve code structure, separation of concerns, and readability.

--- forvoyez-auto-alt-text-for-images.php
+++ forvoyez-auto-alt-text-for-images.php
@@ -35,39 +35,6 @@ function forvoyez_init()
     $settings->init();
 }
 
-function forvoyez_count_incomplete_images()
-{
-    $args = array(
-        'post_type' => 'attachment',
-        'post_mime_type' => 'image',
-        'post_status' => 'inherit',
-        'posts_per_page' => -1,
-        'meta_query' => array(
-            'relation' => 'OR',
-            array(
-                'key' => '_wp_attachment_image_alt',
-                'value' => '',
-                'compare' => '='
-            ),
-            array(
-                'key' => '_wp_attachment_image_alt',
-                'compare' => 'NOT EXISTS'
-            )
-        )
-    );
-
-    $query_images = new WP_Query($args);
-    $incomplete_count = 0;
-
-    foreach ($query_images->posts as $image) {
-        if (empty($image->post_title) || empty(get_post_meta($image->ID, '_wp_attachment_image_alt', true)) || empty($image->post_excerpt)) {
-            $incomplete_count++;
-        }
-    }
-
-    return $incomplete_count;
-}
-
 function forvoyez_get_api_key(){
     $settings = new Forvoyez_Settings();
     return $settings->get_api_key();
---
+++ includes/forvoyez-constants.php
@@ -0,0 +1,5 @@
+<?php
+defined('ABSPATH') || exit;
+
+define('FORVOYEZ_PLUGIN_DIR', plugin_dir_path(__DIR__));
+define('FORVOYEZ_PLUGIN_URL', plugin_dir_url(__DIR__));
