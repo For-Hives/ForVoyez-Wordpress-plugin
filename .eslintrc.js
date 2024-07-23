336ec4e32f79b884473ffa39a42610fc9ef4d3aa
Cinquin Andy <cinquin.andy@gmail.com>
2024-07-23 16:25:09 +0200

    Update: Refactor ESLint configuration - Simplified formatting for better readability - Consolidated object properties for improved maintainability

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    rules: {
-        'prettier/prettier': 'error',
-    },
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': 'error',
+	},
 }
