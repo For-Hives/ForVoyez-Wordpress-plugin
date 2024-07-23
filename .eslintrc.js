a810fd4e9ac54b902112040b8da776f49bfa7410
Cinquin Andy <cinquin.andy@gmail.com>
2024-07-23 16:20:33 +0200

    Update: Improve ESLint configuration - Set environment to support ES2021 features - Extend recommended ESLint and Prettier configurations for better code quality - Specify parser options for latest ECMAScript version and module source type - Add rule to enforce Prettier formatting
    These changes were made to enhance the code quality by configuring ESLint with updated settings and enforcing consistent code formatting using Prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'prettier/prettier': 'error',
+    },
+}
