23fb16ef4252b38920fe7c888457f555ef298bc6
Cinquin Andy <cinquin.andy@gmail.com>
2024-07-24 10:55:20 +0200

    Update PHP CS Fixer configuration - Adjusted indentation for better code readability - Updated rules for coding standards compliance
    The changes were made to enhance the consistency and maintainability of the codebase by aligning with the specified coding standards.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,18 +1,19 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->exclude('vendor')
-    ->exclude('node_modules');
+	->in(__DIR__)
+	->exclude('vendor')
+	->exclude('node_modules');
 
 $config = new PhpCsFixer\Config();
-return $config->setRules([
-    '@PSR2' => true,
-    'array_syntax' => ['syntax' => 'short'],
-    'no_unused_imports' => true,
-    'ordered_imports' => true,
-    'no_extra_blank_lines' => true,
-    'blank_line_before_statement' => true,
-    'method_chaining_indentation' => true,
-])
-    ->setFinder($finder);
+return $config
+	->setRules([
+		'@PSR2' => true,
+		'array_syntax' => ['syntax' => 'short'],
+		'no_unused_imports' => true,
+		'ordered_imports' => true,
+		'no_extra_blank_lines' => true,
+		'blank_line_before_statement' => true,
+		'method_chaining_indentation' => true,
+	])
+	->setFinder($finder);
