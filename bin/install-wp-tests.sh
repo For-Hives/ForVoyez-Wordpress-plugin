3bbfc47f6243779ca56950c5acbd980ba196bd04
Cinquin Andy <cinquin.andy@gmail.com>
2024-07-29 10:05:20 +0200

    Update install-wp-tests.sh script for WordPress testing - Improved handling of WP_VERSION and download logic - Enhanced error checking and messaging for version retrieval - Refactored file download function for better readability - Updated paths and configurations for test suite installation to match specified WordPress version

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -1,26 +1,54 @@
 #!/usr/bin/env bash
 
-# Load environment variables from .env.testing file
-if [ -f ".env.testing" ]; then
-    export $(grep -v '^#' .env.testing | xargs)
+if [ $# -lt 3 ]; then
+    echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version] [skip-database-creation]"
+    exit 1
 fi
 
-# Use environment variables, fallback to arguments if not set
-DB_NAME=${WP_TESTS_DB_NAME:-$1}
-DB_USER=${WP_TESTS_DB_USER:-$2}
-DB_PASS=${WP_TESTS_DB_PASSWORD:-$3}
-DB_HOST=${WP_TESTS_DB_HOST:-$4}
+DB_NAME=$1
+DB_USER=$2
+DB_PASS=$3
+DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
 SKIP_DB_CREATE=${6-false}
 
-WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
-WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
+TMPDIR=${TMPDIR-/tmp}
+TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
+WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
+WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
-# Check if required variables are set
-if [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASS" ] || [ -z "$DB_HOST" ]; then
-    echo "Error: Database configuration is incomplete. Please check your .env.testing file."
-    exit 1
+download() {
+    if [ `which curl` ]; then
+        curl -s "$1" > "$2";
+    elif [ `which wget` ]; then
+        wget -nv -O "$2" "$1"
+    fi
+}
+
+if [[ $WP_VERSION =~ ^[0-9]+\.[0-9]+\$ ]]; then
+    WP_TESTS_TAG="branches/$WP_VERSION"
+elif [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
+    if [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0] ]]; then
+        # version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
+        WP_TESTS_TAG="tags/${WP_VERSION%??}"
+    else
+        WP_TESTS_TAG="tags/$WP_VERSION"
+    fi
+elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
+    WP_TESTS_TAG="trunk"
+else
+    # http serves a single offer, whereas https serves multiple. we only want one
+    download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
+    grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
+    LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
+    if [[ -z "$LATEST_VERSION" ]]; then
+        echo "Latest WordPress version could not be found"
+        exit 1
+    fi
+    WP_TESTS_TAG="tags/$LATEST_VERSION"
 fi
+
+echo "Using WordPress version: $WP_TESTS_TAG"
 
 # Function to run MySQL commands
 run_mysql_command() {
@@ -50,15 +78,6 @@ reset_db_and_user() {
     echo "User $DB_USER created and granted privileges on $DB_NAME."
 }
 
-# Function to download a file
-download() {
-    if [ `which curl` ]; then
-        curl -s "$1" > "$2";
-    elif [ `which wget` ]; then
-        wget -nv -O "$2" "$1"
-    fi
-}
-
 # Function to install WordPress
 install_wp() {
     if [ -d $WP_CORE_DIR ]; then
@@ -82,7 +101,7 @@ install_wp() {
         tar --strip-components=1 -zxmf $TMPDIR/wordpress.tar.gz -C $WP_CORE_DIR
     fi
 
-    download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
+    download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIRwp-content/db.php
 }
 
 # Function to install test suite
@@ -98,11 +117,20 @@ install_test_suite() {
     if [ ! -d $WP_TESTS_DIR ]; then
         # set up testing suite
         mkdir -p $WP_TESTS_DIR
-        svn co --quiet https://develop.svn.wordpress.org/${WP_VERSION}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
-        svn co --quiet https://develop.svn.wordpress.org/${WP_VERSION}/tests/phpunit/data/ $WP_TESTS_DIR/data
+        svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
+        svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/data/ $WP_TESTS_DIR/data
     fi
 
-    create_wp_tests_config
+    if [ ! -f wp-tests-config.php ]; then
+        download https://develop.svn.wordpress.org/${WP_TESTS_TAG}/wp-tests-config-sample.php "$WP_TESTS_DIR"/wp-tests-config.php
+        # remove all forward slashes in the end
+        WP_CORE_DIR=$(echo $WP_CORE_DIR | sed "s:/\+$::")
+        sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR/':" "$WP_TESTS_DIR"/wp-tests-config.php
+        sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" "$WP_TESTS_DIR"/wp-tests-config.php
+        sed $ioption "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR"/wp-tests-config.php
+        sed $ioption "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR"/wp-tests-config.php
+        sed $ioption "s|localhost|${DB_HOST}|" "$WP_TESTS_DIR"/wp-tests-config.php
+    fi
 }
 
 create_wp_tests_config() {
