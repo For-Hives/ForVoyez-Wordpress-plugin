05a5796984d473c54f248dc0d637d93e5f05c8f5
Cinquin Andy <cinquin.andy@gmail.com>
2024-08-01 12:16:22 +0200

    UI: Update tab labels for better accessibility - Added translation functions to tab labels for language support - Improved accessibility by providing descriptive text for screen readers
    These changes were made to enhance the user experience and ensure inclusivity for all users, especially those relying on assistive technologies.

--- templates/main-page.php
+++ templates/main-page.php
@@ -5,15 +5,15 @@ $active_tab = isset($_GET['tab']) ? sanitize_text_field($_GET['tab']) : 'dashboa
     <h1 class="text-2xl font-bold mb-4"><?php echo esc_html(get_admin_page_title()); ?></h1>
 
     <div class="border-b border-gray-200 mt-10">
-        <nav class="-mb-px flex" aria-label="Tabs">
+        <nav class="-mb-px flex" aria-label="<?php esc_attr_e('Tabs', 'forvoyez-auto-alt-text-for-images'); ?>">
             <a href="?page=forvoyez-auto-alt-text&tab=dashboard" class="<?php echo $active_tab == 'dashboard' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'; ?> w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm">
-                Dashboard
+                <?php _e('Dashboard', 'forvoyez-auto-alt-text-for-images'); ?>
             </a>
             <a href="?page=forvoyez-auto-alt-text&tab=manage" class="<?php echo $active_tab == 'manage' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'; ?> w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm">
-                Manage Images
+                <?php _e('Manage Images', 'forvoyez-auto-alt-text-for-images'); ?>
             </a>
             <a href="?page=forvoyez-auto-alt-text&tab=configuration" class="<?php echo $active_tab == 'configuration' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'; ?> w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm">
-                Configuration
+                <?php _e('Configuration', 'forvoyez-auto-alt-text-for-images'); ?>
             </a>
         </nav>
     </div>
