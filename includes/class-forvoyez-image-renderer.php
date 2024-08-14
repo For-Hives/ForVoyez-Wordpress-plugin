a78431726271f8b10d67c8fa1977c6a313a2e1fd
Cinquin Andy <cinquin.andy@gmail.com>
2024-08-14 11:48:11 +0200

    Update UI: Improve button labels and icons
    - Changed button labels to simpler terms for better user understanding
    - Updated button icons for improved visual representation
    - Enhanced UI by removing redundant label display in buttons
    
    These changes were made to enhance the user experience by providing clearer and more visually appealing button labels and icons in the UI.

--- includes/class-forvoyez-image-renderer.php
+++ includes/class-forvoyez-image-renderer.php
@@ -171,8 +171,8 @@ class Forvoyez_Image_Renderer {
         ?>
         <div>
             <div class="-mt-px flex divide-x divide-gray-200">
-                <?php self::render_action_button('analyze',  _e('Analyze', 'forvoyez-auto-alt-text-for-images'), 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01'); ?>
-                <?php self::render_action_button('see-more', _e('Details', 'forvoyez-auto-alt-text-for-images'), 'M15 12a3 3 0 11-6 0 3 3 0 016 0z M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'); ?>
+                <?php self::render_action_button('analyze',  "Analyse", 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01'); ?>
+                <?php self::render_action_button('see-more', "Details", 'M15 12a3 3 0 11-6 0 3 3 0 016 0z M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'); ?>
             </div>
         </div>
         <?php
@@ -190,10 +190,10 @@ class Forvoyez_Image_Renderer {
         ?>
         <div class="flex w-0 flex-1">
             <button class="<?php echo $type; ?>-button relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-xs font-semibold text-gray-900 hover:bg-gray-50">
+                <span><?php echo _e($label, 'forvoyez-auto-alt-text-for-images') ?></span>
                 <svg class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="<?php echo $icon_path; ?>" />
                 </svg>
-                <?php echo esc_html($label); ?>
             </button>
         </div>
         <?php
