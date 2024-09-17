6cc9cf49bed6ab0123f7cf0eb846b2bd93781438
Cinquin Andy <cinquin.andy@gmail.com>
2024-09-17 11:04:33 +0200

    Update text localization for UI elements - Changed translation functions to include esc_html for improved security - Updated text localization for consistency and better user experience

--- includes/class-forvoyez-image-renderer.php
+++ includes/class-forvoyez-image-renderer.php
@@ -118,7 +118,7 @@ class Forvoyez_Image_Renderer {
                 	'auto-alt-text-for-images',
                 );
                 ?>
-                </strong> <span class="title-content"><?php echo esc_html( $image->post_title ?: __( 'Not set', 'auto-alt-text-for-images' ) ); ?></span>
+                </strong> <span class="title-content"><?php echo esc_html( $image->post_title ?: esc_html__( 'Not set', 'auto-alt-text-for-images' ) ); ?></span>
             </p>
             <p class="text-sm text-gray-500 text-left">
                 <strong>
@@ -128,7 +128,7 @@ class Forvoyez_Image_Renderer {
                 	'auto-alt-text-for-images',
                 );
                 ?>
-                </strong> <span class="alt-content"><?php echo esc_html( $image_alt ?: __( 'Not set', 'auto-alt-text-for-images' ) ); ?></span>
+                </strong> <span class="alt-content"><?php echo esc_html( $image_alt ?: esc_html__( 'Not set', 'auto-alt-text-for-images' ) ); ?></span>
             </p>
             <p class="text-sm text-gray-500 text-left">
                 <strong>
@@ -138,7 +138,7 @@ class Forvoyez_Image_Renderer {
                 	'auto-alt-text-for-images',
                 );
                 ?>
-                </strong> <span class="caption-content"><?php echo esc_html( $image->post_excerpt ?: __( 'Not set', 'auto-alt-text-for-images' ) ); ?></span>
+                </strong> <span class="caption-content"><?php echo esc_html( $image->post_excerpt ?: esc_html__( 'Not set', 'auto-alt-text-for-images' ) ); ?></span>
             </p>
         </div>
         <?php
@@ -276,14 +276,14 @@ esc_attr_e(
                 <?php
                 self::render_action_button(
                 	'analyze',
-                	__( 'Analyze', 'auto-alt-text-for-images' ),
+                	esc_html__( 'Analyze', 'auto-alt-text-for-images' ),
                 	'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01',
                 );
                 ?>
                 <?php
                 self::render_action_button(
                 	'see-more',
-                	__( 'Details', 'auto-alt-text-for-images' ),
+                	esc_html__( 'Details', 'auto-alt-text-for-images' ),
                 	'M15 12a3 3 0 11-6 0 3 3 0 016 0z M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z',
                 );
                 ?>
