168ad2ebd9ca10bcd12bbe2af7afdb0b9021c9e6
Cinquin Andy <andy.cinquin@infotel.com>
2024-09-21 13:02:44 +0200

    ✨ Update plugin version to 1.0.1 and project ID in language file
    The commit updates the plugin version constant to 1.0.1 and the Project-Id-Version in the language file to match.

--- auto-alt-text-for-images.php
+++ auto-alt-text-for-images.php
@@ -27,7 +27,7 @@ if ( !defined( 'ABSPATH' ) ) {
 }
 
 // Define plugin constants
-define( 'FORVOYEZ_VERSION', '1.0.0' );
+define( 'FORVOYEZ_VERSION', '1.0.1' );
 define( 'FORVOYEZ_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'FORVOYEZ_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'FORVOYEZ_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
